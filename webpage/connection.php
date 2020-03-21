68acd20944293201702e3750d9f09111f48812af
Azizovashahzoda <shahzoda1998g@gmail.com>
2020-03-21 21:18:57 +0500

    Done

---
+++ webpage/connection.php
@@ -0,0 +1,8 @@
+<?php
+	include('user.php');
+	include('post.php');
+
+	//$db = new PDO('mysql:dbname=blog;host=localhost', 'Azizovashahzoda', '1234');
+	//$usersRepo = new UsersRepo($db);
+	//$postsRepo = new PostsRepo($db);
+?>
